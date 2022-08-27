0b3866c1f20f7a87ec7d160bf43ff658e210b957
milesChild <colechild1@gmail.com>
2022-08-26 20:28:25 -0400

    wip: refractoring architecture #2

---
+++ model/userComms.py
@@ -0,0 +1,13 @@
+# imports
+import csv
+
+# Hub for communication between the user and the algo #
+class userComms:
+
+    def __init__(self):
+        self.log = open('model/log', 'w')
+        self.writer = csv.writer(self.log)
+
+    def transmitMessage(self, msg):
+        self.view.renderMessage(msg)
+        self.writer.writerow(msg)
