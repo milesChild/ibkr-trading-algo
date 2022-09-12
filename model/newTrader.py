c37cbb84341625711256289bd332f2f8e35eb691
milesChild <colechild1@gmail.com>
2022-09-12 12:52:56 -0400

    parital update: newTrader

--- model/newTrader.py
+++ model/newTrader.py
@@ -1,22 +1,53 @@
 from polygon.websocket.models import WebSocketMessage
 from polygon import WebSocketClient
+import csv
 
 
 class newTrader:
     """
     Class to handle mediation of entries, exits, and RM of positions.
     """
-    risk_manager = None  # object that handles rm
-    strategies = None
+    broker = None  # ibkr connection for trade placement
+    tradeQueue = []  # Queue of trades that is filled & emptied on each loop iteration
+    positions = dict()  # OrderID -> Order
+    log = None  # Log that info from runtime will be stored on
+    risk_manager = None  # high-authority object for risk mgmt
+    strategies = None  # list of strategies that will be traded when run
 
     def __init__(self):
         """
         Initialize objects for each instrument-strategy pair and the Polygon websocket object.
         """
+        self.connect_to_broker()
+        self.log = self.init_log()
         self.__initialize_strategies()
         self.last_msg: WebSocketMessage = None
         self.socket_client = WebSocketClient(subscriptions=[
                                              "object should provide a field or a method to efficiently grab all contracts"])
+
+    # Method that initializes a connection between this system and the wrapped ibkr api on another server
+    def connect_to_broker(self):
+        # TODO: Connect to the broker via an api?
+        return
+
+    # Initialize the file to which a log will be written
+    def init_log(self):
+        # open the file in the write mode
+        f = open('ibkr-algo/model', 'w')
+
+        return f
+
+    # Initialize the list of strategies that the system will trade with
+    def __initialize_strategies(self):
+        # TODO
+        return
+
+    # Append a message to the log
+    def append_to_log(self, msg):
+        # create the csv writer
+        writer = csv.writer(self.log)
+        writer.writerow(msg)
+
 
     def start(self):
         """
@@ -44,9 +75,6 @@ class newTrader:
         self.__enter_trades()
         # fourth, manage current positions
         self.__manage_positions()
-
-    def __initialize_strategies(self):
-        return
 
     def __check_time(self):
         """
